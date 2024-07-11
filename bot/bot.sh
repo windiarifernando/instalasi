d76e5e3f638d6a49a45b3c27b76ade3df6d03937
windiarifernando <windiarifernando3@gmail.com>
2024-07-11 15:07:09 +0700

    Add files via upload

--- bot/bot.sh
+++ bot/bot.sh
@@ -18,13 +18,13 @@ NC='\e[0m'
 apt update && apt upgrade
 apt install python3 python3-pip git
 cd /usr/bin
-wget https://raw.githubusercontent.com/windiarifernando29/instalasi/main/bot/bot.zip
+wget https://raw.githubusercontent.com/windiarifernando/instalasi/main/bot/bot.zip
 unzip bot.zip
 mv bot/* /usr/bin
 chmod +x /usr/bin/*
 rm -rf bot.zip
 clear
-wget https://raw.githubusercontent.com/windiarifernando29/instalasi/main/bot/kyt.zip
+wget https://raw.githubusercontent.com/windiarifernando/instalasi/main/bot/kyt.zip
 unzip kyt.zip
 pip3 install -r kyt/requirements.txt
 
